8476e8517c094f2c65d49ddda0f968f3764141f3
Waivern <Toneelvoormarcel@gmail.com>
2021-05-19 14:09:33 +0200

    Create executeSQLScripts.ps1
    Script to run *.sql files

---
+++ executeSQLScripts.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    [string]$sqlFolder,
+    [string]$serverName,
+    [string]$databaseName
+)
+Get-ChildItem -path $sqlFolder -recurse -Filter *.sql | foreach{
+Write-Host "execute: " + $_.FullName
+sqlcmd -S $serverName -U user -P password -d $databaseName -i $_.FullName -b
+}
